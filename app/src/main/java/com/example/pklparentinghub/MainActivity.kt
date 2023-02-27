88d33747a6d4644bdde874a2ad82f84dca7126e2
RazzaGalang <razzagalang45@gmail.com>
2023-02-27 08:16:37 +0700

    Adding Bottom Navigation

--- app/src/main/java/com/example/pklparentinghub/MainActivity.kt
+++ app/src/main/java/com/example/pklparentinghub/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.pklparentinghub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNavigationView) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuBottomNavigationHome -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.menuBottomNavigationEvent -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                R.id.menuBottomNavigationCreateArticle -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                R.id.menuBottomNavigationNotification -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                R.id.menuBottomNavigationProfile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayoutMainActivity,fragment)
+        transaction.commit()
     }
 }
