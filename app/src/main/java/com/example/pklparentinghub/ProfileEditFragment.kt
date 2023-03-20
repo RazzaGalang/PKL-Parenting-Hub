44725dd5ba54f8f2e59c87953bebb3855b59771f
Rasyiid Raafi Shaban <rasyiidraafi05@gmail.com>
2023-03-20 15:21:08 +0700

    Fix Edit Text Layouting

--- app/src/main/java/com/example/pklparentinghub/ProfileEditFragment.kt
+++ app/src/main/java/com/example/pklparentinghub/ProfileEditFragment.kt
@@ -11,12 +11,14 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.pklparentinghub.databinding.FragmentProfileEditBinding
@@ -70,7 +72,17 @@ class ProfileEditFragment : Fragment() {
         back()
         name()
         submit()
+//        photo()
     }
+
+//    private fun photo(){
+//        val pickMedia = registerForActivityResult(Pick()) { -> uri
+//        if (uri != null){
+//            Log.d("PhotoPicker", "Selected URI: $uri")
+//        } else {
+//            Log.d("PhotoPicker", "No media selected")
+//        }}
+//    }
 
     private fun back(){
         binding.editProfileTopBar.setOnClickListener {
